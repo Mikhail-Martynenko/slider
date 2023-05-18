2656d771219f97fe5e6da1719d405f3cd94442bc
Mikhail Martynenko <misha.martynenko8@yandex.ru>
2023-05-18 18:50:38 +0300

    feat: first commit Slider

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Slider from "./Slider/Slider";
+import slides from './Slider/slides.json'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Slider
+                slides={slides}
+                loop
+                navs
+                pages
+                auto
+                stopMouseHover
+                delay={3}
+            />
+        </div>
+    );
 }
 
 export default App;
